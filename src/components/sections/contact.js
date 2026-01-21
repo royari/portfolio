9d223d3a0273adc4056bf2d50568e50782134316
AlphaRoy14 <AlphaRoy14@gmail.com>
2026-01-21 12:34:23 -0500

    Display Apache email below main email

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -91,6 +91,14 @@ const Contact = ({ content }) => {
             <a href={`mailto:${frontmatter.email}`}>
               <Underlining highlight>{frontmatter.email}</Underlining>
             </a>
+            {frontmatter.email2 && (
+              <>
+                <br />
+                <a href={`mailto:${frontmatter.email2}`}>
+                  <Underlining highlight>{frontmatter.email2}</Underlining>
+                </a>
+              </>
+            )}
           </div>
         </div>
         <Social width="9rem" padding="0.5rem 1.25rem" withIcon />
